61d525ab368012e966f7bc1375632eeaedadb7ae
thefarazxr <farazuddinmohammed05@gmail.com>
2023-10-22 17:14:32 -0500

    updated frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -18,7 +18,7 @@ export default function Login() {
 
   const login = (e) => {
     e.preventDefault();
-    fetch(`http://localhost:4000/login`, {
+    fetch(`https://ticktick-lite-mern-gwms.vercel.app/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
