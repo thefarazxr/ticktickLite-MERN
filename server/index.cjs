a596f9ac10d4969bb9c8e6343cfc63a164e2a669
thefarazxr <farazuddinmohammed05@gmail.com>
2023-10-22 19:47:09 -0500

    new exp5

--- server/index.cjs
+++ server/index.cjs
@@ -130,6 +130,9 @@ app.post("/login", async (req, res) => {
     return;
   }
   res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   res.json({
     
     message: "success",
